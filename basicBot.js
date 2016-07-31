74efb15f4b057757e8f038611526d87f69623252
alfa159 <alfa_sc_@hotmail.com>
2016-07-31 16:26:55 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -301,7 +301,7 @@
             opLink: null,
             rulesLink: null,
             themeLink: null,
-            fbLink: 1,
+            fbLink: null,
             youtubeLink: null,
             website: null,
             intervalMessages: [],
